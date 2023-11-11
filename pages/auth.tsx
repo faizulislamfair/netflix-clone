1354ad5290392b7e862140f5fb103ee55fc36eda
faizulislamfair <faizulislamfair@gmail.com>
2023-11-11 18:40:49 +0600

    toggle

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -24,15 +24,21 @@ const Auth = () => {
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                            {variant === 'login' ? 'Sign in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input
-                                label="Username"
-                                onChange={(ev: any) => setName(ev.target.value)}
-                                id="name"
-                                value={name}
-                            />
+
+                            {variant === 'register' && (
+                                <Input
+                                    label="Username"
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id="name"
+                                    value={name}
+                                />
+
+                            )}
+
+
                             <Input
                                 label="Email"
                                 onChange={(ev: any) => setEmail(ev.target.value)}
@@ -49,12 +55,12 @@ const Auth = () => {
                             />
                         </div>
                         <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            Login
+                            {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className='text-neutral-500 mt-12'>
-                            First time using Netflix?
-                            <span className='text-white ml-1 hover:underline cursor-pointer'>
-                                Create an account
+                            {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
+                                {variant === 'login' ? 'Create an account' : 'Login'}
                             </span>
                         </p>
                     </div>
