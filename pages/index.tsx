c2a93fb66a2c0f4cae66a5c7eb5fc51fb0bfaf74
faizulislamfair <faizulislamfair@gmail.com>
2023-11-20 18:18:12 +0600

    fetcher, serverauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-red-500">Netflix Clone</h1>
+      <h1 className="text-4xl text-red-500">Netflix Clone</h1>
+      <button className="-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
