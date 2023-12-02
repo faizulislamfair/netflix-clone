49068eb0350ad6e5ebee6fb3c92f03b618e1c5b7
faizulislamfair <faizulislamfair@gmail.com>
2023-12-02 17:59:02 +0600

    favorites

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { BsFillPlayFill } from "react-icons/bs";
+
+
 interface MovieCardProps {
     data: Record<string, any>;
 }
@@ -43,7 +46,65 @@ const MovieCard: React.FC<MovieCardProps> = ({
                     group-hover:opacity-100
                 "
             >
+                <img
+                    className="
+                cursor-pointer
+                object-cover
+                transition
+                duration
+                shadow-xl
+                rounded-t-md
+                w-full
+                h-[12vw]
+                "
+                    src={data.thumbnailUrl} alt="Thumbnail" />
+                <div
+                    className="
+                z-10
+                bg-zinc-800
+                p-2
+                lg:p-4
+                absolute
+                w-full
+                transition
+                shadow-md
+                rounded-b-md
+                "
+                >
+                    <div className="flex flex-row items-center gap-3">
+                        <div
+                            className="
+                        cursor-pointer
+                        w-6
+                        h-6
+                        lg:w-10
+                        lg:h-10
+                        bg-white
+                        rounded-full
+                        flex
+                        justify-center
+                        items-center
+                        transition
+                        hover:bg-neutral-300
+                        "
+                            onClick={() => { }}>
+                            <BsFillPlayFill size={30} />
+                        </div>
+                    </div>
 
+                    <p className="text-green-400 font-semibold mt-4">
+                        New <span className="text-white">2023</span>
+                    </p>
+
+                    <div className="flex flex-row mt-4 gap-2 items-center">
+                        <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+                    </div>
+
+                    <div className="flex flex-row mt-4 gap-2 items-center">
+                        <p className="text-white text-[10px] lg:text-sm">{data.genre}</p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
